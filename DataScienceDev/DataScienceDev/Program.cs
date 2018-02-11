552fec943cbb23458def07668adb467078967b9c
Blaclix92 <bennymohan08@gmail.com>
2018-02-11 14:41:50 +0100

    New .Net Core console project created

---
+++ DataScienceDev/DataScienceDev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataScienceDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
